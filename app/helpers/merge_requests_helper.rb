853f96c891284956932dd2772164905d1181f1db
Hugo Parente Lima <hugo.pl@gmail.com>
2014-10-09 00:33:14 -0300

    Nice view of a patch :-)

--- app/helpers/merge_requests_helper.rb
+++ app/helpers/merge_requests_helper.rb
@@ -17,4 +17,87 @@ module MergeRequestsHelper
     "pending for #{time}"
   end
 
+  def process_diff diff, &block
+    it = diff.each_line
+    loop do
+      line = it.next
+      next unless line.start_with? '+++'
+      yield DiffFile.new(line, it)
+    end
+  rescue StopIteration
+  end
+
+  private
+
+  class DiffFile
+    def initialize line, it
+      @it = it
+      @name = line[6..-1]
+    end
+
+    attr_reader :name
+
+    def each_line
+      old_ln = 0
+      new_ln = 0
+      loop do
+        diffline = DiffLine.new(@it.next, old_ln, new_ln)
+        yield diffline
+        old_ln, new_ln = diffline.line_numbers
+      end
+    rescue StopIteration
+    end
+  end
+
+  class DiffLine
+    LINE_TYPES = {
+      '@' => :info,
+      '-' => :del,
+      '+' => :add,
+      ' ' => :nil
+    }.freeze
+    def initialize line, old_ln, new_ln
+      @type = (LINE_TYPES[line.first] or :nil)
+
+      if @type == :info
+        @data = line[0..-1]
+        @data =~ /@ -(\d+),\d+ \+(\d+),\d+/
+        @old_ln = $1.to_i
+        @new_ln = $2.to_i
+      else
+        @data = line[1..-2]
+        @old_ln = old_ln
+        @new_ln = new_ln
+
+        @old_ln += 1 unless @type == :add
+        @new_ln += 1 unless @type == :del
+      end
+    end
+
+    attr_reader :data
+
+    def old_ln
+      case @type
+      when :add then ''
+      when :info then '...'
+      else @old_ln
+      end
+    end
+
+    def new_ln
+      case @type
+        when :del then ''
+        when :info then '...'
+      else @old_ln
+      end
+    end
+
+    def line_numbers
+      [@old_ln, @new_ln]
+    end
+
+    def type
+      @type == :nil ? '' : @type.to_s
+    end
+  end
 end
