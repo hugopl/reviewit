fd4e7b01ee92157c40db349a216092c5eb562c32
Hugo Parente Lima <hugo.pl@gmail.com>
2014-10-15 23:58:15 -0300

    Change reviewit MR commit stamp and avoid sending commit stamp inside commit messages on MR update.

--- lib/reviewit/lib/reviewit/action/push.rb
+++ lib/reviewit/lib/reviewit/action/push.rb
@@ -1,11 +1,13 @@
 module Reviewit
   class Push < Action
 
-    RME_STAMP = /^Rme-MR-id: (.*)$/
+    MR_STAMP = 'Reviewit-MR-id:'
+    MR_STAMP_REGEX = /^#{MR_STAMP} (?<id>\d+)$/
 
     def run
       read_commit_header
       read_commit_diff
+      process_commit_message!
 
       if updating?
         api.update_merge_request(@mr_id, @subject, @commit_message, @commit_diff, read_user_message)
@@ -37,19 +39,27 @@ module Reviewit
       @commit_diff = `git show --format=""`
     end
 
+    def process_commit_message!
+      match = MR_STAMP_REGEX.match @commit_message
+      @updating = match
+
+      if @updating
+        @commit_message["#{match}"] = ''
+        @mr_id = match[:id]
+      end
+      @commit_message.strip!
+    end
+
     def append_mr_id_to_commit mr_id
       open('|git commit --amend -F -', 'w+') do |git|
         git.write @commit_message
-        git.write "\n\nRme-MR-id: #{mr_id}\n"
+        git.write "\n\n#{MR_STAMP} #{mr_id}\n"
         git.close
       end
     end
 
     def updating?
-      match = /^Rme-MR-id: (?<id>\d+)$/.match @commit_message
-      return false if match.nil?
-
-      @mr_id = match[:id]
+      @updating
     end
   end
 end
