2602550bfd82541798b5dfae6cb200a6014130b7
Hugo Parente Lima <hugo.pl@gmail.com>
2014-11-18 00:40:14 -0300

    Move WebFonts assets to the right location, so they can be precompiled.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
