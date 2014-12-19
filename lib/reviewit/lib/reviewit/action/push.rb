0f8ca8da03698bf67fb7df865040ffc376b32dec
Hugo Parente Lima <hugo.pl@gmail.com>
2014-12-19 17:23:45 -0300

    Fix patch generation on git 2.1.x.

--- lib/reviewit/lib/reviewit/action/push.rb
+++ lib/reviewit/lib/reviewit/action/push.rb
@@ -33,7 +33,9 @@ module Reviewit
     end
 
     def read_commit_diff
-      @commit_diff = `git show --format=""`
+      patch = `git show --format="%n"`
+      # git 2.1.x returns full patch on git show --format="", so we use %n and remove the lines
+      @commit_diff = patch.lines[3..-1].join
     end
 
     def append_mr_id_to_commit mr_id
