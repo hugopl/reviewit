0f0d5fefbe51e723b825fb0e7ac7b86b30dd9524
Hugo Parente Lima <hugo.pl@gmail.com>
2015-02-12 00:45:10 -0300

    Fix issue #11 - "Your projects: "see merge requests" link is displayed even when there isn't MRs to be displayed."

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,4 +4,10 @@ module ProjectsHelper
     is_ssl = request.ssl?
     "http#{is_ssl ? 's' : ''}://#{request.host}#{port}/api/projects/#{@project.id}/setup"
   end
+
+  def merge_request_count project
+    count = project.merge_requests.pending.count
+    return 'No open merge requests' if count.zero?
+    pluralize(count, 'merge request pending', 'merge requests pending')
+  end
 end
