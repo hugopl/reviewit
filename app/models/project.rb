ea96e3c4faab991cf1da6280316b275625881520
Hugo Parente Lima <hugo.pl@gmail.com>
2015-03-19 00:43:15 -0300

    Add better validation to project repository to avoid shell injection.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
   has_many :merge_requests, dependent: :destroy
 
   validates :name, presence: true
-  validates :repository, presence: true
+  validate :validate_repository
 
   def gitlab_ci?
     !gitlab_ci_token.blank? and !gitlab_ci_project_url.blank?
@@ -36,5 +36,10 @@ class Project < ActiveRecord::Base
     when 'success' then patch.pass!
     when 'failed' then patch.failed!
     end
+  end
+
+  def validate_repository
+    is_valid = URI.regexp =~ repository && /\A[^ ;&|]+\z/ =~ repository
+    errors.add(:repository, 'is not a valid URI') unless is_valid
   end
 end
