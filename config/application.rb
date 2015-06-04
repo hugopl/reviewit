a59976a9eea58a2a7bc98dd866f551860f2096d5
Hugo Parente Lima <hugo.pl@gmail.com>
2015-06-03 23:58:59 -0300

    Move email configuration from the initializer to application.rb.

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,31 @@ module Reviewit
 
     # Configure the status code templates to be served by the app instead of Rack exception application.
     config.exceptions_app = self.routes
+
+    # Generic email configuration
+    unless Rails.env.test?
+      yml = YAML.load_file("#{Rails.root}/config/reviewit.yml")['mail']
+      yml ||= {}
+
+      config.action_mailer.delivery_method = (yml['delivery_method'] || 'file').to_sym
+
+      config.action_mailer.smtp_settings = {
+        address:              yml['address'],
+        port:                 yml['port'],
+        authentication:       yml['authentication'],
+        domain:               yml['domain'],
+        enable_starttls_auto: yml['enable_starttls_auto'],
+        user_name:            yml['user_name'],
+        password:             yml['password'],
+        openssl_verify_mode:  yml['openssl_verify_mode']
+      }
+
+      config.action_mailer.default_url_options = {
+        host: yml['host']
+      }
+      config.action_mailer.default_options = {
+        from: yml['sender']
+      }
+    end
   end
 end
