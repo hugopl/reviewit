4586f5f675d79bc94d4b2852ace4412eb2987724
Hugo Parente Lima <hugo.pl@gmail.com>
2015-08-18 02:19:13 -0300

    Use Minitest::Spec matches instead of ugly assert methods.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'name required' do
     user = build(:user, name: nil)
-    assert_not(user.valid?)
+    user.wont_be :valid?
   end
 
   test 'email required' do
     user = build(:user, email: nil)
-    assert_not user.valid?
+    user.wont_be :valid?
   end
 
   test 'valid email required' do
     user = build(:user, email: 'invalid')
-    assert_not(user.valid?)
+    user.wont_be :valid?
 
     emails = %w(john@example.org
                 john@example.com.br
@@ -27,13 +27,13 @@ class UserTest < ActiveSupport::TestCase
                 barros_filho_washington@silva.br)
     emails.each do |email|
       user = build(:user, email: email)
-      assert(user.valid?, "email #{email} not accepted!?")
+      user.must_be :valid?, "email #{email} not accepted!?"
     end
   end
 
   test 'email uniqueness' do
     user1 = create(:user)
     user2 = build(:user, email: user1.email)
-    assert_not(user2.valid?)
+    user2.wont_be :valid?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
 
 Warden.test_mode!
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
@@ -9,5 +10,6 @@ module ActiveSupport
   class TestCase
     include FactoryGirl::Syntax::Methods
     include Warden::Test::Helpers
+    extend MiniTest::Spec::DSL
   end
 end
