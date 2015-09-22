e80b878ce3a54959e098ed7fe822a2f0292a3737
Hugo Parente Lima <hugo.pl@gmail.com>
2015-09-21 22:58:44 -0300

    Add a nice chart on project page to show MRs created per day in the last 20 days.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -10,4 +10,50 @@ module ProjectsHelper
     return 'No open merge requests' if count.zero?
     pluralize(count, 'merge request pending', 'merge requests pending')
   end
+
+  def projects_mr_chart_data
+    mrs = MergeRequest.group("strftime('%Y-%m-%d', created_at)").where('created_at > ?', 20.days.ago).count
+    last = nil
+    mrs = mrs.inject({}) do |memo, (date, count)|
+      year, month, day = date.split('-').map(&:to_i)
+
+      if last
+        (day - (last + 1)).times do |i|
+          memo["#{year}-#{month}-#{last + i}"] = 0
+        end
+      end
+      memo["#{year}-#{month}-#{day}"] = count
+      last = day
+      memo
+    end
+    {
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: 'Merge requests creation per day (last 20 days)'
+      },
+      xAxis: {
+        categories: mrs.keys,
+        labels: {
+          rotation: -45
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Merge requests created'
+        }
+      },
+      legend: {
+        enabled: true
+      },
+      tooltip: {
+        pointFormat: '{point.y} MRs'
+      },
+      series: [{
+        name: 'Num MRs created',
+        data: mrs.values
+      }]
+    }.to_json.html_safe
+  end
 end
