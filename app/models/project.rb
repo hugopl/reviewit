e203d1cde78c377ede0875a78d56022ec8d65a48
Hugo Parente Lima <hugo.pl@gmail.com>
2015-12-10 17:47:44 -0300

    Fixes the build :-)
    New linter requirements were too strict.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,8 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
   validates :linter, format: { with: %r{\A[^\./](?!.*(\.\.|[&|<>])).*\z},
                                message: "can't have pipes, dots, slashes, two dots, etc, try to use a script in your " \
-                                        'project directory' }
+                                        'project directory' },
+                     allow_blank: true
   validate :validate_repository
 
   def gitlab_ci?
