hugopl/reviewit

commit 8c20c2e0f3c5cb92ee76259246ac4d069f6e9817

Lil refactor on MR controller SQL query.

app/controllers/mr_controller.rb
@@ -2,11 +2,7 @@ class MrController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    project = MergeRequest.find(params[:id]).project
-    if project.users.include? current_user
-      redirect_to "/projects/#{project.id}/merge_requests/#{params[:id]}"
-    else
-      raise ActiveRecord::RecordNotFound, 'Merge request not found.'
-    end
+    project = current_user.projects.joins(:merge_requests).where('merge_requests.id = ?', params[:id])
+    redirect_to("/projects/#{project.id}/merge_requests/#{params[:id]}")
   end
 end

commit 213f792a867a4d91e3f95c18b0436371b355936a

Fixes bug introduced by 8c20c2e0f3c5cb92ee76259246ac4d069f6e9817.

app/controllers/mr_controller.rb
@@ -2,7 +2,8 @@ class MrController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    project = current_user.projects.joins(:merge_requests).where('merge_requests.id = ?', params[:id])
+    project = current_user.projects.joins(:merge_requests).where('merge_requests.id = ?', params[:id]).first
+    raise ActiveRecord::RecordNotFound, 'Merge request not found.' if project.nil?
     redirect_to("/projects/#{project.id}/merge_requests/#{params[:id]}")
   end
 end