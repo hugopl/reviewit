6f5377e4c0493bc478737435192153f8ca71379e
Hugo Parente Lima <hugo.pl@gmail.com>
2016-04-07 15:56:50 -0300

    Add default ordering for comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :patch
 
+  default_scope { order(:created_at) }
   scope :general, -> { where(location: 0) }
   scope :code, -> { where.not(location: 0) }
 end
