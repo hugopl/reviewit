206520bed3e1a130bfbff225e913a03263447e69
Hugo Parente Lima <hugo.pl@gmail.com>
2016-04-07 22:11:48 -0300

    Refactor mailer test to use let calls.

--- test/mailers/merge_request_mailer_test.rb
+++ test/mailers/merge_request_mailer_test.rb
@@ -1,20 +1,23 @@
 require 'test_helper'
 
 class MergeRequestMailerTest < ActionMailer::TestCase
-  def test_mr_creation
+  def setup
     Rails.application.config.action_mailer.default_url_options = { host: 'example.com' }
+  end
 
-    user1 = create(:user, name: 'Mr. Tester')
-    user2 = create(:user)
-    project = create(:project, users: [user1, user2])
-
+  let(:user1)   { create(:user, name: 'Mr. Tester') }
+  let(:user2)   { create(:user) }
+  let(:project) { create(:project, users: [user1, user2]) }
+  let(:mr) do
     mr = build(:merge_request, project: project, subject: 'Subject', author: user1)
-    mr.add_patch(
-      diff: Diff.new(patch('mailer_create_mr')),
-      linter_ok: true,
-      ci_enabled: false)
-    mr.save
+    mr.add_patch(diff: Diff.new(patch('mailer_create_mr')),
+                 linter_ok: true,
+                 ci_enabled: false)
+    mr.save!
+    mr
+  end
 
+  def test_mr_creation
     email = MergeRequestMailer.created(mr).deliver_now
     assert_not ActionMailer::Base.deliveries.empty?
     assert_equal [user2.email], email.to
