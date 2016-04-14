22cdd855156d15a82a80a54e1b57470fcacca78f
Hugo Parente Lima <hugo.pl@gmail.com>
2016-04-14 11:02:32 -0300

    Fix migration used to fix comment positions.
    So it does not breack general comments.

--- db/migrate/20160414021834_fix_comment_locations.rb
+++ db/migrate/20160414021834_fix_comment_locations.rb
@@ -1,9 +1,9 @@
 class FixCommentLocations < ActiveRecord::Migration
   def up
-    Comment.update_all('location = (location - 1)')
+    Comment.where('location > 0').update_all('location = (location - 1)')
   end
 
   def down
-    Comment.update_all('location = (location + 1)')
+    Comment.where('location > 0').update_all('location = (location + 1)')
   end
 end
