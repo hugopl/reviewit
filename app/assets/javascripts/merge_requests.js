18b3e8ba71cb0f7e40b5c69dd27668844f1cfc14
Smith Ascari <smithascari@gmail.com>
2017-01-05 11:32:30 -0300

    Add a hide/show button to file's diff page
    Before the whole file-diff header area was clickable and it was annoying when we need to copy a
    filename e.g. and the diff was hide.

--- app/assets/javascripts/merge_requests.js
+++ app/assets/javascripts/merge_requests.js
@@ -90,12 +90,14 @@ function toogle_merge_requests(){
     var review_items = $('.code-review-item')
     if(review_items.length > 0) {
         review_items.find('.deleted').next('.code-review-container').slideUp(0)
-        review_items.find('.code-review-header').bind('click', function() {
+        review_items.find('.toggle-file-btn').bind('click', function() {
             var container = $(this).parents('.code-review-item').find('.code-review-container');
             if(container.is(':visible')) {
                 container.slideUp();
+                $('.toggle-file-btn').text('Show')
             } else {
                 container.slideDown();
+                $('.toggle-file-btn').text('Hide')
             }
         })
     }
