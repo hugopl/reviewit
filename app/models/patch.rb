57ab2e3490aa732b04cb70a064627355c6a3e872
Hugo Parente Lima <hugo.pl@gmail.com>
2018-02-20 13:53:06 -0300

    Send jobs to CI after commit, not after create.
    This fix issues of sidekiq worker not finding the patch to send to CI.

--- app/models/patch.rb
+++ app/models/patch.rb
@@ -10,7 +10,7 @@ class Patch < ActiveRecord::Base
   validates :diff, presence: true
   validates :commit_message, length: { minimum: 0 }, allow_nil: false
 
-  after_create :push_to_ci_if_needed
+  after_commit :push_to_ci_if_needed, on: :create
 
   delegate :author, to: :merge_request
   delegate :reviewer, to: :merge_request
