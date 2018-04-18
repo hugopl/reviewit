a8fd7c43ffe6502430ab5b5c08012acd50713e35
Hugo Parente Lima <hugo.pl@gmail.com>
2018-04-17 21:19:23 -0300

    Remove leftovers from Tipped.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,6 @@
 require 'digest/md5'
 
 module ApplicationHelper
-  def print_errors(obj)
-    obj_str = obj.to_s
-    obj_instance = instance_variable_get("@#{obj_str}")
-    obj_instance = send(obj) if obj_instance.nil?
-
-    return nil unless obj_instance.errors.any?
-
-    errors = obj_instance.errors.inject('') do |script, error|
-      script + "Tipped.create('##{obj_str}_#{error[0]}', '#{escape_javascript(error[1])}'," \
-               "{ position: 'topright', container: $('#error_tips')[0] });"
-    end
-    script = "$(document).ready(function() {#{errors}});"
-
-    javascript_tag(script)
-  end
-
   def under_index_of?(section)
     path_info.include? section
   end
