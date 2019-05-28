hugopl/reviewit

commit f21ab0412035bbda1d7f01f6690a5ef4c5bc9cb2

Add links to CI builds.

app/helpers/merge_requests_helper.rb
@@ -116,6 +116,18 @@ module MergeRequestsHelper
   end
 
   def ci_icon(patch)
+    return unless patch.project.gitlab_ci?
+
+    icon = %(<i class="#{ci_icon_css(patch)} icon"></i>).html_safe
+    if patch.unknown?
+      icon
+    else
+      content_tag(:a, icon, target: "patch-#{patch.gitlab_ci_hash}",
+                            href: "#{patch.project.gitlab_ci_project_url}/builds/#{patch.gitlab_ci_build}")
+    end
+  end
+
+  def ci_icon_css(patch)
     case patch.gitlab_ci_status
     when 'failed' then 'red remove'
     when 'success' then 'green check'

app/views/merge_requests/_history.html.haml
@@ -32,8 +32,7 @@
                 %td.collapsing= patch.created_at.to_formatted_s(:short)
                 %td.collapsing
                   %i.ui.icon{ class: patch_linter_status(patch) }
-                %td.collapsing
-                  %i.icon{ class: ci_icon(patch) }
+                %td.collapsing= ci_icon(patch)
                 %td.collapsing
                   - if i != last_i
                     .ui.radio.checkbox

app/views/merge_requests/_summary.html.haml
@@ -4,8 +4,7 @@
 
 #summary-bar.ui.five.statistics
   .statistic
-    .value
-      %i.ui.icon{class: ci_icon(@mr.patch)}
+    .value= ci_icon(@mr.patch)
     .label CI status
   .statistic
     .value

commit bd8d83d4aee63974c907899622dc7b50c76e2c9f

Fix CI status icon on list view, broken by f21ab0412035bbda1d7f01f6690a5ef4c5bc9cb2.

app/helpers/merge_requests_helper.rb
@@ -101,7 +101,7 @@ module MergeRequestsHelper
 
   def mr_label(mr)
     if mr.project.gitlab_ci?
-      "<i class=\"#{ci_icon(mr.patch)} icon\"></i> #{mr.target_branch}".html_safe
+      "<i class=\"#{ci_icon_css(mr.patch)} icon\"></i> #{mr.target_branch}".html_safe
     else
       mr.target_branch
     end